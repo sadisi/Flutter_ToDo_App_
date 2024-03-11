1b133d7ce7bee519482849648b034e4c59fdf47f
Devnith Tissera <devnithtissera@gmail.com>
2024-03-11 13:23:13 +0530

    container properties update

--- lib/Screens/memopad_screen.dart
+++ lib/Screens/memopad_screen.dart
@@ -38,7 +38,7 @@ class _NotesPageState extends State<NotesPage> {
         preferredSize: Size.fromHeight(kToolbarHeight),
         child: Container(
           width: double.infinity,
-          color: Colors.red,
+          color: Colors.purple,
           child: AppBar(
             backgroundColor: Colors.transparent,
             title: Center(
@@ -82,6 +82,7 @@ class _NotesPageState extends State<NotesPage> {
           children: <Widget>[
             Container(
               height: 500.0,
+              width: double.infinity,
               child: Expanded(
                 child: FlutterSummernote(
                   hint: 'Your text here...',
@@ -97,8 +98,90 @@ class _NotesPageState extends State<NotesPage> {
               ),
             ),
             Container(
+              padding: EdgeInsets.all(20),
+              color: Colors.purple,
               height: 270,
-              color: Colors.purpleAccent,
+              width: double.infinity,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      height: 45,
+                      width: double.infinity,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 320),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                          ),
+                          onPressed: () {
+                            setState(() {});
+                          },
+                        ),
+                      )),
+                  Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      height: 45,
+                      width: double.infinity,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 320),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                          ),
+                          onPressed: () {
+                            setState(() {});
+                          },
+                        ),
+                      )),
+                  Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      height: 45,
+                      width: double.infinity,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 320),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                          ),
+                          onPressed: () {
+                            setState(() {});
+                          },
+                        ),
+                      )),
+                  Container(
+                      margin: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                      ),
+                      height: 45,
+                      width: double.infinity,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 320),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                          ),
+                          onPressed: () {
+                            setState(() {});
+                          },
+                        ),
+                      )),
+                ],
+              ),
             ),
           ],
         ),
