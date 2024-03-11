e52ea972a41695f1413b594a859ccd48a018ecb1
Devnith Tissera <devnithtissera@gmail.com>
2024-03-11 11:02:27 +0530

    removing the bottom toolbar and creating a container

--- lib/Screens/memopad_screen.dart
+++ lib/Screens/memopad_screen.dart
@@ -1,10 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_summernote/flutter_summernote.dart';
 import 'package:flutter_todo_app/main.dart';
-
 import 'home_screen.dart';
-
-
 
 void main() {
   runApp(MemoPadScreen());
@@ -33,77 +31,91 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+
   GlobalKey<FlutterSummernoteState> _keyEditor = GlobalKey();
   String result = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(kToolbarHeight),
-        child: Container(
-          width: double
-              .infinity, // Set the width to occupy the entire screen width
-          color: Color(0xFFCB84FB),
-          child: AppBar(
-            backgroundColor: Colors
-                .transparent,
-            // Make AppBar transparent to show the custom background color
-            title: Center(
-              child: Text(
-                widget.title,
-                style: TextStyle(
-                  fontSize: 22.0,
-                  color: Colors.black,
-                  fontFamily: 'Roboto-Black',
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+            width: double.infinity,
+            color: Colors.red,
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              title: Center(
+                child: Text(
+                  widget.title,
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.black,
+                    fontFamily: 'Roboto-Black',
+                  ),
                 ),
               ),
-            ),
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()), // Navigate to HomeScreen
-                );
-              },
-            ),
-            elevation: 0,
-            actions: <Widget>[
-              TextButton(
-                onPressed: () async {
-                  final value = (await _keyEditor.currentState?.getText());
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    duration: Duration(seconds: 5),
-                    content: Text(value ?? 'Success',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),),
-                  ));
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            HomePage()), // Navigate to HomeScreen
+                  );
                 },
-                child: Text('Save',
-                style: TextStyle(
-                    color: Colors.black,
-                ),), // Corrected this line
               ),
-
-            ],
+              elevation: 0,
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () async {
+                    final value = (await _keyEditor.currentState?.getText());
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      duration: Duration(seconds: 5),
+                      content: Text(
+                        value ?? 'Success',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ));
+                  },
+                  child: Text(
+                    'Save',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ), // Corrected this line
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-      backgroundColor: Color(0xFFCB84FB),
-      body: FlutterSummernote(
-        hint: 'Your text here...',
-        key: _keyEditor,
-        hasAttachment: true,
-        customToolbar: """
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: FlutterSummernote(
+                hint: 'Your text here...',
+                key: _keyEditor,
+                customToolbar: """
           [
             ['style', ['bold', 'italic', 'underline', 'clear']],
             ['font', ['strikethrough', 'superscript', 'subscript']],
             ['insert', ['link', 'table', 'hr']]
           ]
         """,
-      ),
-    );
+              ),
+            ),
+            Expanded(child: Container(
+              color: Colors.red,
+              width: double.infinity,
+              height: 400,
+
+
+
+            ),),
+          ],
+        ));
   }
 }
